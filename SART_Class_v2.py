e445a76d7bb89bd982b701258e8a56437ffb21c5
Gina Nuss <gina.nuss@gmail.com>
2017-09-04 23:54:31 -0700

    Significant updates to all aspects of code; SART class is now working in v2 .py file; partway through testing with 2016 data

---
+++ SART_Class_v2.py
@@ -0,0 +1,166 @@
+from __future__ import print_function
+import pandas as pd
+import numpy as np
+from collections import defaultdict
+
+
+class SART(object):
+
+    '''Define instance of adventure run tournament class, where a particular
+    bracket format (# of rounds, heats per round, feeding calculations, etc),
+    number of participants, and start times are defined.  This object can be
+    used to run an event, feeding in people's names and times from each round
+    to calculate next round placements and progress through the bracket.
+
+    parameters:
+    r0_seeds - dict, keys are heats for round 1, values are the round 0 or
+        initial seeds that will feed into each round 1 heat.
+    h_pairs - list of tuples, defines pairs of heats grouped together for calculating who
+        moves up vs. down for the next round.
+    fd_into_ht_pairs - list of tuples, defines pairs of heats grouped together that prior
+        heats feed into (i.e. a pair of heats in h_pairs will feed into a pair of heats
+        in fd_into_ht_pairs with the first element of the tuple being for the two heat
+        winners + next three fastest times, and the second element of the tuple being
+        for the remaining 5 times)
+    NEXT THREE ARE CURRENTLY HARD CODED FOR: 80 person bracket, 5 people per heat,
+        and 2 heat winners + next 3 fastest times as method of advancing; will
+        address this flexibility at a future time to add the following parameters:
+        bracket_type - int, represents method of progressing people through rounds
+        heat_size = int, number of people per heat
+        bracket_size = int, total number of people in the bracket
+
+    attributes:
+    round - int, round of tournament
+    heat - int, heat within given round
+    h_seed_key
+    '''
+
+    def __init__(self, r0_seeds, h_pairs, fd_into_ht_pairs, columns, start_times):
+        self.r0_seeds = r0_seeds
+        self.h_pairs = h_pairs
+        self.fd_into_ht_pairs = fd_into_ht_pairs
+        self.columns = columns
+        self.start_times = start_times
+        self.h_seed_key = defaultdict(lambda: [], self.r0_seeds)
+        self.bef_aft_ht_pairs = zip(self.h_pairs, self.fd_into_ht_pairs)
+        self.sd_h_key = None
+        self.bracket_df = None
+        self.tt_df_cleaned = None
+
+    def _define_ht_sd_keys(self, pri_heat_pair1, pri_heat_pair2, win_heat, lose_heat):
+        self.h_seed_key['Heat {}'.format(win_heat)].append('W{}'.format(pri_heat_pair1))
+        self.h_seed_key['Heat {}'.format(win_heat)].append('W{}'.format(pri_heat_pair2))
+
+        for i in range(1,4):
+            self.h_seed_key['Heat {}'.format(win_heat)].append('{}/{}-Q{}'.format(pri_heat_pair1, pri_heat_pair2, i))
+
+        for i in range(4,9):
+            self.h_seed_key['Heat {}'.format(lose_heat)].append('{}/{}-Q{}'.format(pri_heat_pair1, pri_heat_pair2, i))
+
+    def _assign_ht_sd_keys(self):
+        for pri_heats, wn_ls_heats in self.bef_aft_ht_pairs:
+            pri_heat1 = pri_heats[0]
+            pri_heat2 = pri_heats[1]
+            win_ht = wn_ls_heats[0]
+            lose_ht = wn_ls_heats[1]
+            self._define_ht_sd_keys(pri_heat1, pri_heat2, win_ht, lose_ht)
+
+    def _assign_flip_sd_ht_keys(self):
+        self.sd_h_key = {val:key for key, value in self.h_seed_key.iteritems() for i, val in enumerate(value)}
+
+    def prepare_sd_h_keys(self):
+        self._assign_ht_sd_keys()
+        self._assign_flip_sd_ht_keys()
+
+    def create_bracket(self):
+        RH_index = [i for i in self.sd_h_key.keys()]
+        self.bracket_df = pd.DataFrame(index=RH_index, columns=self.columns)
+        self.bracket_df.index.name = 'Seed'
+
+        for val in self.bracket_df.index:
+            self.bracket_df.loc[val,['Nxt_Heat']] = self.sd_h_key[val]
+            self.bracket_df.loc[val,['Round']] = int(str(self.bracket_df.loc[val,['Nxt_Heat']]).split()[2][0])-1
+
+        for key, time in self.start_times.iteritems():
+            self.bracket_df['Nxt_Heat_Time'][self.bracket_df['Nxt_Heat'] == key] = time
+
+        self.bracket_df.sort_index(inplace=True)
+
+    def process_time_trial_results(self, raw_csv):
+        time_trial_df = pd.read_csv(raw_csv)
+        self.tt_df_cleaned = time_trial_df[['Surname', 'First name', 'Time']]
+        self.tt_df_cleaned = self.tt_df_cleaned.rename(index=str, columns={'First name': 'First_name'})
+        self.tt_df_cleaned = self.tt_df_cleaned.sort_values(['Time'])
+        self.tt_df_cleaned['Seed'] = [int(i)+1 for i in self.tt_df_cleaned.index]
+
+    def add_time_trial_results(self):
+        for val in self.tt_df_cleaned['Seed']:
+            self.bracket_df.loc[val,['First_name']] = (self.tt_df_cleaned['First_name'][self.tt_df_cleaned['Seed'] == val].item())
+            self.bracket_df.loc[val,['Surname']] = (self.tt_df_cleaned['Surname'][self.tt_df_cleaned['Seed'] == val].item())
+            self.bracket_df.loc[val,['Time']] = (self.tt_df_cleaned['Time'][self.tt_df_cleaned['Seed'] == val].item())
+
+    def clean_results_csv(self, raw_csv):
+        df = pd.read_csv(raw_csv)
+        df = df[['Surname', 'First name', 'Time', 'Entry cl. No']]
+        df = df.rename(index=str, columns={'First name': 'First_name'})
+        df = df.rename(index=str, columns={'Entry cl. No': 'Heat'})
+        df = df.sort_values(['Time'])
+        return df
+
+    def nxt_ht_assigns(self, results_df, rnd, person_info=['Surname', 'First_name', 'Time']):
+        '''define inputs, how function works'''
+        for ht1, ht2 in self.h_pairs:
+            if ht1 < (rnd * 100 + 100):
+                temp_df1 = results_df[(results_df['Heat'] == ht1)].sort_values('Time')
+                temp_df1.reset_index(drop=True, inplace=True)
+                temp_df2 = results_df[(results_df['Heat'] == ht2)].sort_values('Time')
+                temp_df2.reset_index(drop=True, inplace=True)
+
+                for colnm in person_info:
+                    ht_win1 = temp_df1[colnm][temp_df1['Time'] == temp_df1['Time'].min()].item()
+                    self.bracket_df.loc['W{}'.format(ht1),[colnm]] = ht_win1
+                    ht_win2 = temp_df2[colnm][temp_df2['Time'] == temp_df2['Time'].min()].item()
+                    self.bracket_df.loc['W{}'.format(ht2),[colnm]] = ht_win2
+
+                temp_df1.drop(0, inplace=True)
+                temp_df2.drop(0, inplace=True)
+                temp_dftot = pd.concat([temp_df1, temp_df2]).sort_values('Time')
+                temp_dftot.reset_index(drop=True, inplace=True)
+
+                for i, row in enumerate(temp_dftot.values):
+                    for j, colnm in enumerate(person_info):
+                        self.bracket_df.loc['{}/{}-Q{}'.format(ht1, ht2, i+1),[colnm]] = row[j]
+
+    def print_heat_assigns(self, rnd):
+        info_list = []
+
+        for heat in xrange((rnd*100 + 1), (rnd*100 + 17)):
+            df = self.bracket_df[self.bracket_df['Nxt_Heat'] == 'Heat {}'.format(heat)].sort_values('Time')
+            info_list.append('Heat {}'.format(heat))
+            info_list.append('\n')
+            info_list.append(df[['First_name', 'Surname']])
+            info_list.append('\n')
+
+        return info_list
+
+    def assign_nxt_ht_to_ss_import_csv(self, csv_imp_file, prior_rnd):
+        ss_input = pd.read_csv(csv_imp_file)
+        ss_input = ss_input[pd.notnull(ss_input['Surname'])]
+        ss_input = ss_input[ss_input['Surname'] != 'Winterbottom']
+        bracket_df_temp = self.bracket_df[(pd.notnull(self.bracket_df['Surname'])) & (self.bracket_df['Round'] == prior_rnd)]
+
+        if len(ss_input) != len(bracket_df_temp):
+            return 'Warning: mismatch between next round import file and current round results lists - check data!'
+        else:
+            for val in ss_input.index:
+                ss_input.loc[val, ['Long']] = bracket_df_temp[
+                    (bracket_df_temp['Round'] == prior_rnd) &
+                    (bracket_df_temp['Surname'] == ss_input.loc[
+                    val, ['Surname']].values[0]) & (bracket_df_temp[
+                    'First_name'] == ss_input.loc[
+                    val, ['First name']].values[0])]['Nxt_Heat'].values[0]
+
+                ss_input.loc[val, ['Short']] = 'H' + ss_input.loc[val, ['Long']].values[0][-3:]
+                ss_input.loc[val, ['Cl. no.']] = ss_input.loc[val, ['Long']].values[0][-3:]
+
+            return ss_input
--- bracket_calcs.py
+++ bracket_calcs.py
@@ -16,62 +16,73 @@ class SART(object):
     parameters:
     r0_seeds - dict, keys are heats for round 1, values are the round 0 or
         initial seeds that will feed into each round 1 heat.
-    h_pairs - dict, defines pairs of heats grouped together for calculating who
+    h_pairs - list of tuples, defines pairs of heats grouped together for calculating who
         moves up vs. down for the next round.
-    fd_into_ht_pairs -
-    round - int, round of tournament
-    heat - int, heat within given round
-    runners - list, names of runners in given heat
-        **runners may be a dictionary with times too...need to think about flow
-    bracket_type - int, represents method of progressing people through rounds
-    heat_size = int, number of people per heat
-    bracket_size = int, total number of people in the bracket
+    fd_into_ht_pairs - list of tuples, defines pairs of heats grouped together that prior
+        heats feed into (i.e. a pair of heats in h_pairs will feed into a pair of heats
+        in fd_into_ht_pairs with the first element of the tuple being for the two heat
+        winners + next three fastest times, and the second element of the tuple being
+        for the remaining 5 times)
+    NEXT THREE ARE CURRENTLY HARD CODED FOR: 80 person bracket, 5 people per heat,
+        and 2 heat winners + next 3 fastest times as method of advancing; will
+        address this flexibility at a future time to add the following parameters:
+        bracket_type - int, represents method of progressing people through rounds
+        heat_size = int, number of people per heat
+        bracket_size = int, total number of people in the bracket
 
     attributes:
-    TBD
+    round - int, round of tournament
+    heat - int, heat within given round
+    h_seed_key
     '''
 
-    def __init__(self, round, heat, runners, bracket_type):
-        self.round = round
-        self.heat = heat
-        self.runners = runners
-        self.bracket_type = bracket_type
+    def __init__(self, r0_seeds, h_pairs, fd_into_ht_pairs, columns):
+        self.r0_seeds = r0_seeds
+        self.h_pairs = h_pairs
+        self.fd_into_ht_pairs = fd_into_ht_pairs
+        self.columns = columns
+        self.h_seed_key = defaultdict(lambda: [], self.r0_seeds)
+        self.bef_aft_ht_pairs = zip(sorted(zip(self.h_pairs, self.fd_into_ht_pairs)
+        self.sd_h_key = None
+        self.bracket_df = None
 
-    def define_ht_sd_keys(pri_heat_pair1, pri_heat_pair2, win_heat, lose_heat):
-        h_seed_key['Heat {}'.format(win_heat)].append('W{}'.format(pri_heat_pair1))
-        h_seed_key['Heat {}'.format(win_heat)].append('W{}'.format(pri_heat_pair2))
+    def _define_ht_sd_keys(self, pri_heat_pair1, pri_heat_pair2, win_heat, lose_heat):
+        self.h_seed_key['Heat {}'.format(win_heat)].append('W{}'.format(pri_heat_pair1))
+        self.h_seed_key['Heat {}'.format(win_heat)].append('W{}'.format(pri_heat_pair2))
 
         for i in range(1,4):
-            h_seed_key['Heat {}'.format(win_heat)].append('{}/{}-Q{}'.format(
+            self.h_seed_key['Heat {}'.format(win_heat)].append('{}/{}-Q{}'.format(
             pri_heat_pair1, pri_heat_pair2, i))
 
         for i in range(4,9):
-            h_seed_key['Heat {}'.format(lose_heat)].append('{}/{}-Q{}'.format(
+            self.h_seed_key['Heat {}'.format(lose_heat)].append('{}/{}-Q{}'.format(
             pri_heat_pair1, pri_heat_pair2, i))
 
-    def assign_ht_sd_keys(r0_seeds, bef_aft_ht_pairs):
+    def _assign_ht_sd_keys(self):
 
-        h_seed_key = defaultdict(lambda: [], r0_seeds)
-
-        for pri_heats, wn_ls_heats in bef_aft_ht_pairs:
+        for pri_heats, wn_ls_heats in self.bef_aft_ht_pairs:
             pri_heat1 = pri_heats[0]
             pri_heat2 = pri_heats[1]
             win_ht = wn_ls_heats[0]
             lose_ht = wn_ls_heats[1]
-            define_ht_sd_keys(pri_heat1, pri_heat2, win_ht, lose_ht)
+            self._define_ht_sd_keys(pri_heat1, pri_heat2, win_ht, lose_ht)
 
-    def assign_flip_sd_ht_keys(h_seed_key):
-        sd_h_key = {val:key for key, value in h_seed_key.iteritems()
+    def _assign_flip_sd_ht_keys(self):
+        self.sd_h_key = {val:key for key, value in self.h_seed_key.iteritems()
             for i, val in enumerate(value)}
 
-    def create_bracket(sd_h_key, columns):
-        RH_index = [i for i in sd_h_key.keys()]
-        bracket_df = pd.DataFrame(index=RH_index, columns=columns)
-        bracket_df.index.name = 'Seed'
+    def prepare_sd_h_keys(self):
+        self._assign_ht_sd_keys()
+        self._assign_flip_sd_ht_keys()
 
-        for val in bracket_df.index:
-            bracket_df.loc[val,['Nxt_Heat']] = sd_h_key[val]
-            bracket_df.loc[val,['Round']] = int(str(bracket_df.loc[
+    def create_bracket(self):
+        RH_index = [i for i in self.sd_h_key.keys()]
+        self.bracket_df = pd.DataFrame(index=RH_index, columns=self.columns)
+        self.bracket_df.index.name = 'Seed'
+
+        for val in self.bracket_df.index:
+            self.bracket_df.loc[val,['Nxt_Heat']] = self.sd_h_key[val]
+            self.bracket_df.loc[val,['Round']] = int(str(self.bracket_df.loc[
                 val,['Nxt_Heat']]).split()[2][0])-1
 
     def process_time_trial_results(raw_csv):
@@ -104,21 +115,102 @@ if __name__ == '__main__':
     'Heat 115': [6, 27, 38, 59, 70], 'Heat 116': [11, 22, 43, 54, 75]}
 
     #heat pairs round 1 - 4:
-    h_pairs = {101: 102, 103: 104, 105: 106, 107: 108, 109: 110, 111:112, 113: 114, 115: 116,
-    201: 203, 205: 207, 209: 211, 213: 215, 202: 204, 206: 208, 210: 212, 214: 216,
-    301: 302, 303: 304, 305: 306, 307: 308, 309: 310, 311: 312, 313: 314, 315: 316,
-    401: 402, 403: 404, 405: 406, 407: 408, 409: 410, 411: 412, 413: 414, 415: 416}
+    h_pairs = [(101, 102), (103, 104), (105, 106), (107, 108), (109, 110),
+    (111, 112), (113, 114), (115, 116), (201, 203), (205, 207), (209, 211),
+    (213, 215), (202, 204), (206, 208), (210, 212), (214, 216), (301, 302),
+    (303, 304), (305, 306), (307, 308), (309, 310), (311, 312), (313, 314),
+    (315, 316), (401, 402), (403, 404), (405, 406), (407, 408), (409, 410),
+    (411, 412), (413, 414), (415, 416)]
 
     #heat pairs round 2 - 5 that prior round feed into e.g. 101, 102 feed into 201, 202:
-    fd_into_ht_pairs = [(201, 202), (203, 204), (205, 206), (207, 208), (209, 210), (211, 212),
-    (213, 214), (215, 216), (316, 312), (315, 311), (314, 310), (313, 309),
-    (308, 304), (307, 303), (306, 302), (305, 301), (403, 401), (404, 402),
-    (407, 405), (408, 406), (411, 409), (412, 410), (415, 413), (416, 414),
-    (502, 501), (504, 503), (506, 505), (508, 507), (510, 509), (512, 511),
-    (514, 513), (516, 515)]
-
-    #combined heat data for full bracket flow through:
-    bef_aft_ht_pairs = zip(sorted(zip(h_pairs.keys(), h_pairs.values())), fd_into_ht_pairs)
+    fd_into_ht_pairs = [(201, 202), (203, 204), (205, 206), (207, 208),
+    (209, 210), (211, 212), (213, 214), (215, 216), (316, 312), (315, 311),
+    (314, 310), (313, 309), (308, 304), (307, 303), (306, 302), (305, 301),
+    (403, 401), (404, 402), (407, 405), (408, 406), (411, 409), (412, 410),
+    (415, 413), (416, 414), (502, 501), (504, 503), (506, 505), (508, 507),
+    (510, 509), (512, 511), (514, 513), (516, 515)]
 
     #Bracket columns:
-    columns = ['Round', 'Nxt_Heat', 'First_name', 'Surname', 'Time']
+    columns = ['Round', 'Nxt_Heat', 'First_name', 'Surname', 'Time', 'Nxt_Heat_Time']
+
+    #Heat start times:
+    start_times = {'Heat 101': '12:00pm',
+             'Heat 102': '12:03pm',
+             'Heat 103': '12:06pm',
+             'Heat 104': '12:09pm',
+             'Heat 105': '12:12pm',
+             'Heat 106': '12:15pm',
+             'Heat 107': '12:18pm',
+             'Heat 108': '12:21pm',
+             'Heat 109': '12:24pm',
+             'Heat 110': '12:27pm',
+             'Heat 111': '12:30pm',
+             'Heat 112': '12:33pm',
+             'Heat 113': '12:36pm',
+             'Heat 114': '12:39pm',
+             'Heat 115': '12:42pm',
+             'Heat 116': '12:45pm',
+             'Heat 201': '3:00pm',
+             'Heat 202': '3:03pm',
+             'Heat 203': '3:06pm',
+             'Heat 204': '3:09pm',
+             'Heat 205': '3:12pm',
+             'Heat 206': '3:15pm',
+             'Heat 207': '3:18pm',
+             'Heat 208': '3:21pm',
+             'Heat 209': '3:24pm',
+             'Heat 210': '3:27pm',
+             'Heat 211': '3:30pm',
+             'Heat 212': '3:33pm',
+             'Heat 213': '3:36pm',
+             'Heat 214': '3:39pm',
+             'Heat 215': '3:42pm',
+             'Heat 216': '3:45pm',
+             'Heat 301': '8:00pm',
+             'Heat 302': '8:03pm',
+             'Heat 303': '8:06pm',
+             'Heat 304': '8:09pm',
+             'Heat 305': '8:12pm',
+             'Heat 306': '8:15pm',
+             'Heat 307': '8:18pm',
+             'Heat 308': '8:21pm',
+             'Heat 309': '8:24pm',
+             'Heat 310': '8:27pm',
+             'Heat 311': '8:30pm',
+             'Heat 312': '8:33pm',
+             'Heat 313': '8:36pm',
+             'Heat 314': '8:39pm',
+             'Heat 315': '8:42pm',
+             'Heat 316': '8:45pm',
+             'Heat 401': '9:15am',
+             'Heat 402': '9:18am',
+             'Heat 403': '9:21am',
+             'Heat 404': '9:24am',
+             'Heat 405': '9:27am',
+             'Heat 406': '9:30am',
+             'Heat 407': '9:33am',
+             'Heat 408': '9:36am',
+             'Heat 409': '9:39am',
+             'Heat 410': '9:42am',
+             'Heat 411': '9:45am',
+             'Heat 412': '9:48am',
+             'Heat 413': '9:51am',
+             'Heat 414': '9:54am',
+             'Heat 415': '9:57am',
+             'Heat 416': '10:00am',
+             'Heat 501': '11:15am',
+             'Heat 502': '11:18am',
+             'Heat 503': '11:21am',
+             'Heat 504': '11:24am',
+             'Heat 505': '11:27am',
+             'Heat 506': '11:30am',
+             'Heat 507': '11:33am',
+             'Heat 508': '11:36am',
+             'Heat 509': '11:39am',
+             'Heat 510': '11:42am',
+             'Heat 511': '11:45am',
+             'Heat 512': '11:48am',
+             'Heat 513': '11:51am',
+             'Heat 514': '11:54am',
+             'Heat 515': '11:57am',
+             'Heat 516': '12:00pm'}
